049ec7c5aaf336b23cf9cc8e438957f594c23dc9
exactlyprateek <prateekrohilla4.pr@gmail.com>
2020-03-29 04:19:07 +0530

    Components added for table and navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
+import MiniDrawer from "./NavBar";
 
 export default class App extends Component {
   render() {
-    return <div>Hello</div>;
+    return <MiniDrawer />;
   }
 }
